bd7eb08a9a53a94c5db48b60dc4713dba090dbb0
shayansalehi1381 <shayansalehisut@gmail.com>
2024-03-10 22:53:44 +0330

    general cours for each department in main branch added

--- src/University/Department/ComputerEngDepartment.java
+++ src/University/Department/ComputerEngDepartment.java
@@ -1,6 +1,7 @@
 package University.Department;
 
 import University.Course.Course;
+import University.Course.GeneralCourse;
 import University.Course.SpecializedCourse;
 
 import java.time.DayOfWeek;
@@ -16,12 +17,10 @@ public class ComputerEngDepartment extends Department {
     public static ArrayList<Course> CECourses = new ArrayList<>();
     public ComputerEngDepartment(){
         super();
-        course1 = new SpecializedCourse("BasicProgramming",21,"Sharifi",100,4,false, DayOfWeek.MONDAY, LocalTime.of(10,0),LocalTime.of(13,0), LocalDate.of(2024,6,20));
-        CECourses.add(course1);
-        course2 = new SpecializedCourse("AdvancedProgramming ",22,"Rezaii",80,4,false, DayOfWeek.TUESDAY,LocalTime.of(14,0),LocalTime.of(16,0), LocalDate.of(2024,6,21));
-        CECourses.add(course2);
-        course3 = new SpecializedCourse("DataStrocture",23,"Ahmadi",50,4,false, DayOfWeek.WEDNESDAY,LocalTime.of(10,0),LocalTime.of(12,0), LocalDate.of(2024,6,18));
-        CECourses.add(course3);
+        CECourses.add(new SpecializedCourse("BasicProgramming",21,"Sharifi",100,4,false, DayOfWeek.MONDAY, LocalTime.of(10,0),LocalTime.of(13,0), LocalDate.of(2024,6,20)));
+        CECourses.add(new SpecializedCourse("AdvancedProgramming ",22,"Rezaii",80,4,false, DayOfWeek.TUESDAY,LocalTime.of(14,0),LocalTime.of(16,0), LocalDate.of(2024,6,21)));
+        CECourses.add(new SpecializedCourse("DataStrocture",23,"Ahmadi",50,4,false, DayOfWeek.WEDNESDAY,LocalTime.of(10,0),LocalTime.of(12,0), LocalDate.of(2024,6,18)));
+        CECourses.add(new GeneralCourse("Islamic ethics",10,"Moradi",25,2,true, DayOfWeek.TUESDAY,LocalTime.of(13,0),LocalTime.of(14,0), LocalDate.of(2024,6,20)));
 
         for (Course c:CECourses){
             Department.allCourses.add(c);
--- src/University/Department/MathDepartment.java
+++ src/University/Department/MathDepartment.java
@@ -1,6 +1,7 @@
 package University.Department;
 
 import University.Course.Course;
+import University.Course.GeneralCourse;
 import University.Course.SpecializedCourse;
 
 import java.time.DayOfWeek;
@@ -18,6 +19,7 @@ public class MathDepartment extends Department {
         mathCourses.add(new SpecializedCourse("calcalus1",11,"Jamali",100,4,false, DayOfWeek.SUNDAY,LocalTime.of(10,0),LocalTime.of(13,0), LocalDate.of(2024,6,20)));
         mathCourses.add(new SpecializedCourse("calcalus2",12,"Habibi",80,4,false, DayOfWeek.MONDAY,LocalTime.of(14,0),LocalTime.of(16,0), LocalDate.of(2024,6,20)));
         mathCourses.add(new SpecializedCourse("probability",13,"Mirsadeqi",0,4,false, DayOfWeek.SUNDAY,LocalTime.of(10,0),LocalTime.of(12,0), LocalDate.of(2024,6,17)));
+        mathCourses.add(new GeneralCourse("Farsi",10,"Jalili",25,3,true, DayOfWeek.SATURDAY,LocalTime.of(13,0),LocalTime.of(15,0), LocalDate.of(2024,6,11)));
         for (Course c:mathCourses){
             Department.allCourses.add(c);
         }
